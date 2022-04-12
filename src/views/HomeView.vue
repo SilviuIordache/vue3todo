343c55a0737373355bfba97d408731d4ff9c8dba
silviu <silviu.iordache92@gmail.com>
2022-04-12 16:16:05 +0300

    Add ls persistance

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,12 +18,34 @@
 <script setup>
 import AddToDo from "@/components/AddToDo.vue";
 import ToDoList from "@/components/ToDoList.vue";
-
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 const items = ref([]);
+
+onMounted(() => {
+  const storage = localStorage.getItem("toDoItems");
+
+  if (storage) {
+    const storageItems = JSON.parse(storage);
+    items.value = storageItems;
+  } else {
+    localStorage.setItem("toDoItems", JSON.stringify([]));
+  }
+});
 function addToDo(item) {
   items.value.push(item);
+
+  // update local storage
+  const storage = localStorage.getItem("toDoItems");
+  if (storage) {
+    const newItems = JSON.parse(storage);
+    newItems.push(item);
+    localStorage.setItem("toDoItems", JSON.stringify(newItems));
+  } else {
+    const newStorage = [];
+    newStorage.push(item);
+    localStorage.setItem("toDoItems", newStorage);
+  }
 }
 
 function toggleCompletion(id) {
