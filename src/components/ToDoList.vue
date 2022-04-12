f2a0ede002b59cc6207937652f88003dd1694698
silviu <silviu.iordache92@gmail.com>
2022-04-12 14:26:33 +0300

    Refactor with use props

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,16 +1,14 @@
 <template lang="">
   <div class="card px-3 py-5">
     <div class="card-body">
-      <div v-for="(item, index) in items" :key="index">
+      <div v-for="(item, index) in props.items" :key="index">
         {{ index + 1 }}. {{ item }}
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  props: ["items"],
-};
+<script setup>
+const props = defineProps({ items: Array });
 </script>
 <style lang=""></style>
